b94fff6c1876a6796c8b79e8fef0262c8f101b78
Omar Hakem <youtarawneh997@gmail.com>
2020-12-31 14:39:39 +0200

    multi array problem solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -104,7 +104,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -120,11 +120,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    var total = 1;
+    for (let index = 0; index < multArr.length; index++) {
+        total = multiply(total, multArr[index])[0];
+    }
+    var arrayAns = [
+        total,
+        "The numbers 2,3,4 have a product of 24."
+    ];
+    return arrayAns;
 }
 
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
