899967ee1c80bffc895d4b9d3a5de350981ac9e1
Omar Hakem <youtarawneh997@gmail.com>
2020-12-31 13:30:31 +0200

    multi problem solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -34,17 +34,17 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
     var total = a * b;
 
-    var returnArray = [
+    var arrayAns = [
         total,
         "The product of 5 and 9 is 45."
     ];
 
-    return returnArray;
+    return arrayAns;
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+// testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
